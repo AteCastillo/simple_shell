1f2cd3b55da2ee20c544484c0a00ca736fb298ac
mi-kei-la <micaela.percovich@gmail.com>
2020-11-19 21:33:53 +0000

    add main file

--- main.c
+++ main.c
@@ -13,16 +13,13 @@ int main(void)
 	char **argv, *string;
 	size_t size = 0;
 
-	/* memory assignations */
-	string = malloc(sizeof(char) * 512);
+	string = malloc(sizeof(char) * 512); /* memory assignations */
 	argv = malloc(sizeof(char *) * 64);
-	for (i = 0; i < 64, i++) /* assign memory to argv pointers */
+	for (i = 0; i < 64, i++)
 		argv[i] = malloc(sizeof(char) * 64);
-
 	while (1) /* loop is broken by user input */
 	{
 		string = _get_newline(string);
-
 		if (string == NULL) /* check for EOF */
 		{
 			write(1, "\n", 1);
@@ -33,12 +30,9 @@ int main(void)
 			write(1, "\n", 1);
 			continue;
 		}
-
 		argv = tokenizer(string); /* load argv with tokens */
-
-		child_pid = fork()
-
-		if (child_pid == -1); /* check return of fork */
+		child_pid = fork();
+		if (child_pid == -1) /* check return of fork */
 			continue;
 		else if (child_pid == 0) /* execute command checking for errors */
 		{
@@ -49,12 +43,9 @@ int main(void)
 		else
 			wait(&status);
 	}
-
-	/* free all memory */
-	free(string);
+	free(string); /* free memory */
 	for (i = 0; i < 64; i++)
 		free(argv[i]);
 	free(argv);
-
 	return (0);
 }
