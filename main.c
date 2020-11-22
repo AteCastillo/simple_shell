f9b3e1591e8256b03b0db59391437ff43ac14d44
Atenea <atecast@gmail.com>
2020-11-22 21:37:10 +0000

    main without _getnewline

--- main.c
+++ main.c
@@ -8,22 +8,51 @@
 
 int main(void)
 {
-	int i, status, ret /*, fd = 0, a */;
+	int i, status, ret; /*ret for return from getline*/
 	pid_t child_pid;
 	size_t size = 0;
 	char **argv = NULL, *string = NULL;
 
 	while (1) /* loop is broken by user input */
 	{
-/*		a = isatty(fd);
-		if (a == 1)
-*/			write(1, "($) ", 4);
+		_prompt();
 		ret = getline(&string, &size, stdin);
-		string = _getnewline(string, ret);
-		if (string == NULL) /* check for EOF */
+		/*	string = _getnewline(string, ret);*/
+		/*		if (string == NULL)
+		{
+			free(string);
 			break;
-		else if (string[0] == '\n') /* input is just a return */
+			}*/
+		if (ret == -1)
+		{
+			if (ret == EOF)
+			{
+				free(string);
+				return(0);
+			}
+			free(string);
+			perror("error");
+			return(1);
+		}
+		if (string[0] == '\n') /* input is just a return */
+		{
+			free(string);
 			continue;
+		}
+
+		if (_strcmp(string, "exit\n") == 0)
+			{
+				free(string);
+				return(0);
+			}
+		if (_strcmp(string, "env\n") == 0)
+		{
+			_printenv();
+			free(string);
+			continue;
+		}
+		ret = _strlen(string);
+		string[ret - 1] = '\0';
 		argv = tokenize(string); /* load argv with tokens */
 		child_pid = fork();
 		if (child_pid == -1) /* check return of fork */
@@ -40,9 +69,21 @@ int main(void)
 		}
 		else
 			wait(&status);
+		free(string);
+		for (i = 0; argv != NULL; i++)
+			free(argv[i]);
+		free(argv);
 	}
-/*	free(string);
-	for (i = 0; argv != NULL; i++)
-		free(argv[i]);
-*/	return (0);
+	return (0);
+}
+
+/**
+ * main - for interactive mode
+ * Return: nothing
+ */
+
+void _prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(1, "($) ", 4);
 }
